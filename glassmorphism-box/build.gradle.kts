3f1b6c4ab8d08974b0f1fd7a6003fd0eef663a9b
Taufik Kurahman <out1994@fifgroup.co.id>
2024-10-11 17:37:49 +0700

    Bump version to 1.0.4

--- glassmorphism-box/build.gradle.kts
+++ glassmorphism-box/build.gradle.kts
@@ -43,7 +43,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.taufik-kurahman",
         artifactId = "glassmorphism-box",
-        version = "1.0.3"
+        version = "1.0.4"
     )
     pom {
         name.set("glassmorphism-box")
